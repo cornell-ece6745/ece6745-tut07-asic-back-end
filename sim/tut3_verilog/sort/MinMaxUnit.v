fdb410697572633c3833377cdaf9eefdd11cbc74
cb535 <cb535@cornell.edu>
2025-02-18 12:12:37 -0500

    staff update from: ece6745-tut07-asic-back-end-0.0-1392-gddcc2c4 [ci skip]

--- sim/tut3_verilog/sort/MinMaxUnit.v
+++ sim/tut3_verilog/sort/MinMaxUnit.v
@@ -21,7 +21,31 @@ module tut3_verilog_sort_MinMaxUnit
   // This model is incomplete. As part of the tutorial you will insert
   // logic here to implement the min/max unit. You should also write a
   // unit test from scratch named MinMaxUnit_test.py.
-  // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''\/
+
+  always_comb begin
+
+    // Find min/max
+
+    if ( in0 >= in1 ) begin
+      out_max = in0;
+      out_min = in1;
+    end
+    else if ( in0 < in1 ) begin
+      out_max = in1;
+      out_min = in0;
+    end
+
+    // Handle case where there is an X in the input
+
+    else begin
+      out_min = 'x;
+      out_max = 'x;
+    end
+
+  end
+
+  // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''/\
 
 endmodule
 
